6b561ad57432b8dd36eee491c15aaf318c056068
gwennbouteloup <gwennbouteloup@gmail.com>
2016-11-29 14:20:06 +0100

    commit before testing Oleose template

---
+++ src/main/java/urban/agriculture/web/mvc/config/ConstantsUrban.java
@@ -0,0 +1,14 @@
+package urban.agriculture.web.mvc.config;
+
+import java.io.File;
+
+/**
+ * Here are the definition of the project specific constants
+ *  
+ * @author gbouteloup
+ *
+ */
+public class ConstantsUrban {
+	public static final String resourcesForWebViewLocation = "/webapp" + File.separator + "WEB-INF" + File.separator
+			+ "views" + File.separator + "resources";
+}
--- src/main/java/urban/agriculture/web/mvc/config/WebMvcConfig.java
+++ src/main/java/urban/agriculture/web/mvc/config/WebMvcConfig.java
@@ -14,10 +14,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	// equivalents for <mvc:resources/> tags
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//		registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
-//		registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
-//		registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
-		registry.addResourceHandler("resources/**").addResourceLocations("/webapp/WEB-INF/views/resources/").setCachePeriod(31556926);
+		registry.addResourceHandler("resources/**").addResourceLocations(ConstantsUrban.resourcesForWebViewLocation)
+				.setCachePeriod(31556926);
 	}
 
 	// equivalent for <mvc:default-servlet-handler/> tag
